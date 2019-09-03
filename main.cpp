c61449f8f0e6d5650eae86c0e24ecb1f700ea730
vkmouse <knight31241@gmail.com>
2019-09-03 09:27:00 +0800

    modify readme and delete .exe

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-    string algorithm = argv[1];
+    string algorithm = argv[1]; // algorithm
     search_algorithm *search_alg;
 
     if (algorithm == "hc")
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
         );
 
     search_alg->init(
-        argv[2],       //dataset_path
+        argv[2],       // dataset_path
         atoi(argv[3]), // num_iter
         atoi(argv[4]), // max_evaluation_time
         atoi(argv[5]), // per_evaluation_time
